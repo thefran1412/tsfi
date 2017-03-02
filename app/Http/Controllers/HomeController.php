a2ec2b2d5c6af574add1f762feb7528acbf2e8fe
Francesc Edo Trias <Francesc Edo Trias>
2017-03-02 16:23:55 +0100

    dashboard created

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('backend.dashboard');
+    }
+    public function resource()
+    {
+        return view('backend.addresource');
     }
 }

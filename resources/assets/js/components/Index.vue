1d3e6611a8f078ee36a56db8fd62cffa20bdaed6
Carl Ruiz <carl.45.cr@gmail.com>
2017-03-05 01:42:43 +0100

    create footer

--- resources/assets/js/components/Index.vue
+++ resources/assets/js/components/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="app">
+    <div id="appVue">
         <header>
             <div class="header-top-item header-search-container">
                 <span>TSFI</span>
@@ -34,6 +34,33 @@
         <div class="container">
             <router-view></router-view>
         </div>
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 network">
+                        <h4>Segueix-nos a :</h4>
+                        <a href="#"><img src="/img/twitter.svg"></a>
+                        <a href="#"><img src="/img/facebook.svg"></a>
+                        <a href="#"><img src="/img/instagram.svg"></a>
+                    </div>
+                    <div class="col-md-offset-2 col-md-4 contact">
+                        <h4>Contacta amb nosaltres :</h4>
+                        <span>Número de Telefón: 93 333 33 33</span>
+                        <span>Correu Electrónic: 
+                            <a href="mailto:info@tsfi.com">info@tsfi.com</a>
+                        </span>
+                    </div>
+                </div>
+                <div class="row entitats">
+                    <div class="col-md-12">
+                        <h4>Entitats Col·laboradores :</h4>
+                    </div>
+                    <div class="col-md-4" v-for="e in entities">
+                        <a v-bind:href="e.url" target="_blank">{{ e.name }}</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
 </template>
 
@@ -73,6 +100,38 @@
                     { name: 'Sinatra' },
                     { name: 'Laravel' },
                     { name: 'Phoenix' }
+                  ],
+                entities: [
+                    { name: 'ADECAT', url: 'http://www.adecat.org/' },
+                    { name: 'Ajuntament de BCN - Barcelona Activa', url: 'http://www.barcelonactiva.cat' },
+                    { name: "Ajuntament de BCN - Institut Municipal d'Informàtica", url: 'http://ajuntament.barcelona.cat/imi/ca' },
+                    { name: 'Anna Ginorella de Mundet', url: 'http://agmundet.es/wp/' },
+                    { name: 'ASEITEC - CECOT', url: 'http://www.aseitec.org/' },
+                    { name: 'Cambra de Comerç de BCN', url: 'http://www.cambrabcn.org/es/' },
+                    { name: "Col.legi d'Enginyers Industrials de Catalunya - COEIC", url: 'http://www.eic.cat/' },
+                    { name: 'Comissons Obreres - CCOO', url: 'http://www.ccoo.cat/' },
+                    { name: "Consorci d'Educació", url: 'http://www.edubcn.cat/ca/' },
+                    { name: 'Fira de Barcelona', url: 'http://www.firabarcelona.com/ca' },
+                    { name: 'Fundació BCN FP', url: 'http://www.fundaciobcnfp.cat' },
+                    { name: 'Fundació EURECAT', url: 'http://www.adecat.org/' },
+                    { name: 'Gencat - ACCIÓ', url: 'http://accio.gencat.cat/cat/' },
+                    { name: "Gencat - Departament d'Ensenyament", url: 'http://ensenyament.gencat.cat/ca/inici/' },
+                    { name: "Gencat - Direcció General d'Industria", url: 'http://empresa.gencat.cat/ca/inici/' },
+                    { name: "Gencat - Empresa i Coneixement CTTI", url: 'http://empresa.gencat.cat/ca/inici/' },
+                    { name: 'Grup SIMON', url: 'http://www.grupsimon.com/ca' },
+                    { name: 'Gutmar', url: 'http://www.gutmar.com/index.php?lang=ca' },
+                    { name: 'Industria21', url: 'http://www.industria21.cat/' },
+                    { name: 'Institut Escola del Treball', url: 'http://www.escoladeltreball.org/ca' },
+                    { name: 'Jesuïtes Clot', url: 'http://www.clot.fje.edu/' },
+                    { name: 'LEITAT', url: 'http://www.leitat.org/castellano/' },
+                    { name: 'Pacte Industrial de la Regió Metropolitana de BCN', url: 'http://www.pacteindustrial.org/' },
+                    { name: 'PIMEC', url: 'https://www.pimec.org/' },
+                    { name: 'Salessians de Sarrià', url: 'http://www.salesianssarria.com/' },
+                    { name: 'SANDVIK', url: 'http://www.home.sandvik/en/' },
+                    { name: 'Schneider', url: 'http://www.schneider-electric.es/es/' },
+                    { name: 'SEAT', url: 'http://www.seat.es' },
+                    { name: 'Unió General dels Treballadors - UGT', url: 'http://www.ugt.cat/' },
+                    { name: 'Unió Patronal Metal.lúrgica - UPM', url: 'http://www.upm.org.es/' }
                   ]
             }
         },
@@ -99,9 +158,13 @@
 
                 var typeNum = localStorage.getItem("numType");
 
+                console.log(typeNum);
+
                 if(localStorage.length === 2 && Number(typeNum) === 0){
 
                     var typeUser = localStorage.getItem("typeUser");
+
+                    console.log(typeNum);
 
                     localStorage.removeItem("numType");
 
@@ -124,6 +187,7 @@
 
                 localStorage.setItem("numType", 0);
 
+                this.whatUserPage();
                 this.typeUser();
             },
             nameWithLang ({ name }) {

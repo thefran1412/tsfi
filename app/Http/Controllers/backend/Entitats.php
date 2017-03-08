5e8ca159d69dbc6a962301862533de39358f4769
Francesc Edo Trias <Francesc Edo Trias>
2017-03-08 17:39:26 +0100

    logout button

--- app/Http/Controllers/backend/Entitats.php
+++ app/Http/Controllers/backend/Entitats.php
@@ -7,6 +7,16 @@ use App\Http\Controllers\Controller;
 
 class Entitats extends Controller
 {
+     protected $loginPath = '/admin/login';  
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
     	return view('backend.entitats.index');
--- app/Http/Controllers/backend/Recursos.php
+++ app/Http/Controllers/backend/Recursos.php
@@ -7,6 +7,16 @@ use App\Http\Controllers\Controller;
 
 class Recursos extends Controller
 {
+     protected $loginPath = '/admin/login';  
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 	public function index()
 	{
 		return view('backend.recursos.index');

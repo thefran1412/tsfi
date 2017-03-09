109ed389832eb3e30f5acd84d5c87c21e4978e8a
Adrián Gallego <gallego.adrian@gmail.com>
2017-03-09 15:32:36 +0100

    mod Resources

--- resources/assets/js/components/Resources.vue
+++ resources/assets/js/components/Resources.vue
@@ -10,7 +10,7 @@
 				<h2>PROVA SUBTITOL De la plantilla del recurs de proves i bla, bla, bla, bla, bla...</h2>
 
 				<!-- class="descshort" -->
-				<p> <strong>DESCRIPCIO CURTA </strong> amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,</p>
+				<p> <strong>DESCRIPCIO CURTA </strong> am wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,</p>
 					<!-- desc1 -->
 				<p> <strong>DESCRIPCIO LLARGA 1! </strong> sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a 1</p>
 					<!-- desc2 -->
@@ -18,17 +18,33 @@
 			</div>
 
 			<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 resource-extras">
-				<h1 class="title-data-sheet">FITXA TÈCNICA</h1>
-				<div class="dateini">date('dataInici');</div>
-				<div class="dateend">date('DdtaFinal');</div>
-				<div class="free">boolean('Gratuit');</div>
-				<div class="priceshort"> double('preuInferior', 15, 2);</div>
-				<div class="pricelong">double('preuSuperior', 15, 2);</div>
-				<div class="dateini">dateTime('dataPublicacio');</div>
-				<div class="autor">string('creatPer');</div>
-				<div class="loc">integer('idLocalitzacio')</div>
+				<h1>FITXA TÈCNICA</h1>
+				<div>
+					<h3>Data inicial i final</h3>
+					<p>08/03/2017</p>
+					<p>31/03/2021</p>
+				</div>
+				<div>
+					<h3>Preu</h3>
+					<p>gratuit</p>
+					<p>0 €</p>
+					<p>2500 €</p>
+				</div>
+				<div>
+					<h3>Data de publicació</h3>
+					<p>07/03/2017</p>
+				</div>
+				<div class="extras">
+					<h3>Publicat per</h3>
+					<p>Adrián Gallego Luque</p>
+				</div>
+				<div class="extras">
+					<h3>Direcció</h3>
+					<p>Calle estroncio 15-21, 3-4</p>
+				</div>
+				
 				<div class="banners">
-
+					<h1>Banner</h1>
 				</div>
 			</div>
 		</div>

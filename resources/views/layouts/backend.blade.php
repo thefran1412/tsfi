9d038cdfafbe8e9a544f9c50dfb3538913a5258f
Nico <nicofviteri@gmail.com>
2017-03-13 17:58:08 +0100

    backend scripts

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -10,7 +10,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-    <script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
 
     <!-- Styles -->
     <link href="/css/backend.css" rel="stylesheet">

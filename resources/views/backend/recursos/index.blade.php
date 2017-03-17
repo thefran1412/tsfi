75a0482170f50f686b6e7c1941466866db941557
Francesc Edo Trias <Francesc Edo Trias>
2017-03-17 17:23:53 +0100

    Backend Style v.2

--- resources/views/backend/recursos/index.blade.php
+++ resources/views/backend/recursos/index.blade.php
@@ -2,11 +2,13 @@
 
 @section('titol', 'Recursos')
 @section('css')
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css">
+    {{-- <link rel="stylesheet" href="https://cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css"> --}}
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.13/css/dataTables.bootstrap.min.css">
 @endsection
 
 @section('script')
     <script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.13/js/dataTables.bootstrap.min.js"></script>
     <script>
         $(document).ready(function() {
              $('#aprovados').DataTable();
@@ -17,68 +19,80 @@
 @endsection
 
 @section('content')
-
-    <div class="content" id="listrecurso">
-        <h2>Pendientes</h2>
-        <table id="pendientes" class="table table-bordered table-responsive">
-            <thead >
-                <tr>
-                    <th>Titol</th>
-                    <th>Subtitol</th>
-                    <th>Descripció</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($pendents as $pendent)
+    <div class="wrapper" id="listrecurso">
+        <div class="section">
+            <div class="sectionHeader">
+                <h2>Pendientes</h2>
+                <i class="fa fa-angle-down"></i>    
+            </div>
+            <div class="sectionBody">
+                <table id="pendientes" class="table table-striped" cellspacing="0" width="100%">
+                    <thead >
+                        <tr>
+                            <th>Titol</th>
+                            <th>Subtitol</th>
+                            <th>Autor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($pendents as $pendent)
+                            <tr>
+                                <th><a href="{{action('backend\Recursos@edit', ['id' => $pendent->recurs_id])}}">{{$pendent->titolRecurs}}</a></th>
+                                <th>{{$pendent->subTitol}}</th>
+                                <th>{{$pendent->creatPer}}</th>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="section">
+            <h2>Reportados</h2>
+            <hr>
+            <table id="reportados" class="table table-bordered table-responsive">
+                <thead >
                     <tr>
-                        <th><a href="{{action('backend\Recursos@edit', ['id' => $pendent->recurs_id])}}">{{$pendent->titolRecurs}}</a></th>
-                        <th>{{$pendent->subTitol}}</th>
-                        <th>{{$pendent->descDetaill1}}</th>
+                        <th>Titol</th>
+                        <th>Subtitol</th>
+                        <th>Autor</th>
+                        <th>Actions</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-        <h2>Reportados</h2>
-        <table id="reportados" class="table table-bordered table-responsive">
-            <thead >
-                <tr>
-                    <th>Titol</th>
-                    <th>Subtitol</th>
-                    <th>Descripció</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($resources as $resource)
+                </thead>
+                <tbody>
+                    @foreach($resources as $resource)
+                        <tr>
+                            <th><a href="{{action('backend\Recursos@edit', ['id' => $resource->recurs_id])}}">{{$resource->titolRecurs}}</a></th>
+                            <th>{{$resource->subTitol}}</th>
+                            <th>{{$resource->creatPer}}</th>
+                            <th>Hola</th>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="section">
+            <h2>Aprovados</h2>
+            <hr>
+            <table id="aprovados" class="table table-bordered table-responsive">
+                <thead >
                     <tr>
-                        <th><a href="{{action('backend\Recursos@edit', ['id' => $resource->recurs_id])}}">{{$resource->titolRecurs}}</a></th>
-                        <th>{{$resource->subTitol}}</th>
-                        <th>{{$resource->descDetaill1}}</th>
-                        <th>Hola</th>
+                        <th>Titol</th>
+                        <th>Subtitol</th>
+                        <th>Autor</th>
+                        <th>Actions</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-        <h2>Aprovados</h2>
-        <table id="aprovados" class="table table-bordered table-responsive">
-            <thead >
-                <tr>
-                    <th>Titol</th>
-                    <th>Subtitol</th>
-                    <th>Descripció</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($resources as $resource)
-                    <tr>
-                        <th><a href="{{action('backend\Recursos@edit', ['id' => $resource->recurs_id])}}">{{$resource->titolRecurs}}</a></th>
-                        <th>{{$resource->subTitol}}</th>
-                        <th>{{$resource->descDetaill1}}</th>
-                        <th>Hola</th>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach($resources as $resource)
+                        <tr>
+                            <th><a href="{{action('backend\Recursos@edit', ['id' => $resource->recurs_id])}}">{{$resource->titolRecurs}}</a></th>
+                            <th>{{$resource->subTitol}}</th>
+                            <th>{{$resource->creatPer}}</th>
+                            <th>Hola</th>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -13,6 +13,7 @@
     <!-- Styles -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/backend.css" rel="stylesheet">
+    <link href="/css/font-awesome.css" rel="stylesheet">
     @yield('css')
 
 </head>
@@ -23,10 +24,10 @@
 {{--         <div class="headerSearch">
         </div> --}}
         <div class="headerActions">
-            <a href="{{url('/')}}">Frontend</a>
-            <a href="#" class="noti"><img src="/img/noti.png"></a>
+            <a title="Veure Pàgina" href="{{url('/')}}" class="home"><img src="/img/home.png"></a>
+            <a title="Notificacions" href="#" class="noti"><img alt="Notificacions" src="/img/noti.png"></a>
             {{-- LOGOUT --}}
-            <a href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">admin<img src="/img/down.png" class="dropdown"></a>
+            <a title="Més opcions" href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">admin<img src="/img/down.png" class="dropdown"></a>
             <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
                 {{ csrf_field() }}
             </form>
@@ -35,17 +36,17 @@
     {{-- HEADER END --}}
     <div class="menu">
         <ul class="nav navbar-nav">
-            <li><a href="{{  action('backend\Backend@index') }}">Inici</a></li>
-            <li><a href="{{  action('backend\Recursos@index') }}">Recursos</a></li>
-            <li><a href="{{  action('backend\Recursos@add') }}">---Add</a></li>
-            <li><a href="{{  action('backend\Categories@index') }}">---Categories</a></li>
-            <li><a href="{{  action('backend\Tags@index') }}">---Tags</a></li>
-            <li><a href="{{  action('backend\Entitats@index') }}">Entitats</a></li>
-            <li><a href="{{  action('backend\Entitats@add') }}">---Add</a></li>
-            <li><a href="{{  action('backend\Usuaris@index') }}">Usuaris</a></li>
-            <li><a href="{{  action('backend\Usuaris@add') }}">---Add</a></li>
-            <li><a href="{{  action('backend\Analytics@index') }}">Analytics</a></li>
-            <li><a href="{{  action('backend\Backend@config') }}">Configuració</a></li>
+            <li><a href="{{  action('backend\Backend@index') }}"><i class="fa fa-home"></i>Inici</a></li>
+            <li><a href="{{  action('backend\Recursos@index') }}"><i class="fa fa-file-text"></i>Recursos</a></li>
+            {{-- <li><a href="{{  action('backend\Recursos@add') }}">---Add</a></li> --}}
+            <li><a href="{{  action('backend\Categories@index') }}"><i class="fa fa-archive"></i>Categories</a></li>
+            <li><a href="{{  action('backend\Tags@index') }}"><i class="fa fa-tag"></i>Tags</a></li>
+            <li><a href="{{  action('backend\Entitats@index') }}"><i class="fa fa-building"></i>Entitats</a></li>
+            {{-- <li><a href="{{  action('backend\Entitats@add') }}">---Add</a></li> --}}
+            <li><a href="{{  action('backend\Usuaris@index') }}"><i class="fa fa-user"></i>Usuaris</a></li>
+            {{-- <li><a href="{{  action('backend\Usuaris@add') }}">---Add</a></li> --}}
+            <li><a href="{{  action('backend\Analytics@index') }}"><i class="fa fa-bar-chart"></i>Analytics</a></li>
+            <li><a href="{{  action('backend\Backend@config') }}"><i class="fa fa-cog"></i>Configuració</a></li>
         </ul>
     </div>
     <div class="content">

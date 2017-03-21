a382f8c9330a4701c424cae7e66450cbf03dd5e0
Francesc Edo Trias <francesc.edo.trias@gmail.com>
2017-03-21 17:47:19 +0100

    css + map in create entity

--- resources/views/backend/entitats/add.blade.php
+++ resources/views/backend/entitats/add.blade.php
@@ -12,31 +12,43 @@
      <div class="create">
             {!!Form::open(['action' => 'backend\Entitats@store', 'method' => 'post'])!!}
               <div class="paper">
-                  {!!Form::label('nom', 'Nom: ')!!}
-                  {!!Form::text('nom', null, ['class' => 'form-control', 'placeholder' => 'Nom de la entitat'])!!}
+                  <div>
+                    {!!Form::label('nom', 'Nom: ')!!}
+                    {!!Form::text('nom', null, ['class' => 'form-control', 'placeholder' => 'Nom de la entitat'])!!}
+                  </div>
+                  <div>
+                    {!!Form::label('link', 'Enllaç pàgina web: ')!!}
+                    {!!Form::text('link', null, ['class' => 'form-control', 'placeholder' => 'Enllaç a la pàgina web'])!!}
+                  </div>
+                  <div>
+                    {!!Form::label('desc', 'És membre: ')!!}
+                    {!!Form::checkbox('esMembre', null, ['class' => 'form-control', 'placeholder' => 'Es membre'])!!}
+                  </div>
               </div>
               <div class="paper">
-                  {!!Form::label('desc', 'Telèfon 1: ')!!}
-                  {!!Form::number('telf1', null, ['class' => 'form-control', 'placeholder' => 'Telefon de la entitat 1'])!!}
+                  <div>
+                    {!!Form::label('desc', 'Telèfon 1: ')!!}
+                    {!!Form::number('telf1', null, ['class' => 'form-control', 'placeholder' => 'Telefon de la entitat 1'])!!}
+                  </div>
+                  <div>
+                    {!!Form::label('desc', 'Telèfon 2: ')!!}
+                    {!!Form::number('telf2', null, ['class' => 'form-control', 'placeholder' => 'Telefon de la entitat 2'])!!}
+                  </div>
+                </div>
+                <div class="paper">
+                  <div class="upload">
+                    {!!Form::label('desc', 'Logo: ')!!}
+                    {!!Form::file('logo', null, ['class' => 'form-control', 'placeholder' => 'Logo'])!!}
+                  </div>
 
-                  {!!Form::label('desc', 'Telèfon 2: ')!!}
-                  {!!Form::number('telf2', null, ['class' => 'form-control', 'placeholder' => 'Telefon de la entitat 2'])!!}
-                  {!!Form::label('desc', 'Enllaç pàgina web: ')!!}
-                  {!!Form::text('link', null, ['class' => 'form-control', 'placeholder' => 'Enllaç a la pàgina web'])!!}
-
-                  {!!Form::label('desc', 'Logo: ')!!}
-                  {!!Form::file('logo', null, ['class' => 'form-control', 'placeholder' => 'Logo'])!!}
-
-                  {!!Form::label('desc', 'És membre: ')!!}
-                  {!!Form::checkbox('esMembre', null, ['class' => 'form-control', 'placeholder' => 'Es membre'])!!}
               </div>
               <div class="paper">
                   {!!Form::label('desc', 'Adreça: ')!!}
-                  {!!Form::text('adreca', null, ['class' => 'form-control', 'placeholder' => 'Adreça de la entitat'])!!}
+                  {!!Form::text('adreca', null, ['class' => 'form-control location', 'placeholder' => 'Adreça de la entitat'])!!}
 
-                  localització
+                  <iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJhwzf5aSipBIRVKZl89WuF0c&key=AIzaSyC4_7SNfNuNIVeW8vwguS8QheTX0v3Bll0" allowfullscreen></iframe>
               </div>
-              <div class="paper">
+              <div class="air">
                   {!!Form::submit('Guardar canvis', ['class' => 'btn btn-primary'])!!}
               </div>
           {!!Form::close()!!}

60c5e1d9cbdc080c8a4ab3c23ac4eb6b17c9343a
Adrián Gallego <gallego.adrian@gmail.com>
2017-03-24 18:23:57 +0100

    add new conditions Resources.vue

--- resources/assets/js/components/Resources.vue
+++ resources/assets/js/components/Resources.vue
@@ -6,11 +6,9 @@
 			<div v-if="resource" class="col-xs-12 col-sm-8 col-md-8 col-lg-8 resource-body">
 				<!--  clearfix visible-xs-block visible-sm-block visible-md-block visible-lg-block -->
 				<h1>{{resource[0].titolRecurs}}</h1>
-				<img class="img-responsive" :src="'/img/image/'+ resource[0].fotoResum" alt="prueba View">
+				<img class="img-responsive" :src="'/img/image/'+ resource[0].fotoResum" :alt="resource[0].titolRecurs" :title="resource[0].titolRecurs">
 				<!-- width="960" height="540" -->
-
 				<h2>{{resource[0].subtitol}}</h2>
-				<!-- <pre>{{this.recurso}}</pre> -->
 				<!-- class="descshort" -->
 				<p><strong>{{resource[0].descBreu}}</strong></p>
 					<!-- desc1 -->
@@ -21,18 +19,22 @@
 
 			<div v-if="resource" class="col-xs-12 col-sm-4 col-md-4 col-lg-4 resource-extras">
 				<h1>FITXA TÈCNICA</h1>
-				<div class="resource-extras-tag">
+				<div v-if="resource[0].age" class="resource-extras-tag">
+					<h3>Edats recomenades</h3>
+					<p v-for="(r, key) in resource[0].age">{{r.codiEdat}}</p>
+				</div>
+				<div v-if="dateIni && dateEnd"class="resource-extras-tag">
 					<h3>Data inicial i final</h3>
 					<p>{{dateIni}}</p>
 					<p>{{dateEnd}}</p>
 				</div>
-				<div class="resource-extras-tag">
+				<div v-if="resource[0].gratuit === 1 || (resource[0].preuInferior && resource[0].preuSuperior)" class="resource-extras-tag">
 					<h3>Preu</h3>
 					<p v-if="resource[0].gratuit === 1">gratuït</p>
 					<p v-if="resource[0].gratuit === 0">{{resource[0].preuInferior}} €</p>
 					<p v-if="resource[0].gratuit === 0">{{resource[0].preuSuperior}} €</p>
 				</div>
-				<div class="extras resource-extras-tag">
+				<div v-if="resource[0].creatPer && datePub"class class="extras resource-extras-tag">
 					<h3>Publicat per</h3>
 					<p>{{resource[0].creatPer}}</p>
 					<h3>Data de publicació</h3>
@@ -43,13 +45,13 @@
 					<h3>Direcció</h3>
 					<p>Calle estroncio 15-21, 3-4</p>
 					<h3>Xarxes Socials</h3>
-						<a class="socialMedia" :href="'https://twitter.com/'+ resource[0].entity[0].twitter">
+						<a class="socialMedia" :href="'https://twitter.com/'+ resource[0].entity[0].twitter" target="_blank" :title="'https://twitter.com/'+ resource[0].entity[0].twitter">
 						   <i class="fa fa-twitter fa-3x"></i>
 						</a>
-						<a class="socialMedia" :href="'https://www.facebook.com/'+ resource[0].entity[0].facebook">
+						<a class="socialMedia" :href="'https://www.facebook.com/'+ resource[0].entity[0].facebook" target="_blank" :title="'https://twitter.com/'+ resource[0].entity[0].twitter">
 						  <i class="fa fa-facebook fa-3x"></i>
 						</a>
-						<a class="socialMedia" :href="'https://www.instagram.com/'+ resource[0].entity[0].instagram">
+						<a class="socialMedia" :href="'https://www.instagram.com/'+ resource[0].entity[0].instagram" target="_blank" :title="'https://twitter.com/'+ resource[0].entity[0].twitter">
 						  <i class="fa fa-instagram fa-3x"></i>
 						</a>
 				</div>
@@ -84,7 +86,6 @@
 					this.dateEnd = response.data.dateEnd;
 					this.datePub = response.data.datePub;
 					console.log(this.resource);
-					//console.log(this.recurso);
 					console.log(this.dateIni);
 					console.log(this.dateEnd);
 					console.log(this.datePub);

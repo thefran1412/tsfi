ef381824a4a1a9100fa98b6c72a5a290253f4e93
toshmir <nicofviteri@gmail.com>
2017-04-19 14:41:10 +0200

    usuaris style

--- resources/views/backend/config.blade.php
+++ resources/views/backend/config.blade.php
@@ -4,7 +4,17 @@
   <i class="fa fa-angle-right"></i>
   <a href="{{ action('backend\Backend@config') }}">Configuració</a>
 @endsection
+@section('css')
+    <link href="{{ URL::asset('/css/backend/add.css') }}" rel="stylesheet">
+@endsection
 
 @section('content')
-     <h2><a href="{{ action('backend\Usuaris@index') }}">Usuaris</a></h2>
+
+    <div class="paper">
+        <div class="paperfull">
+            <button class="btn btn-default">
+                <h2><a style=" text-decoration: none;" href="{{ action('backend\Usuaris@index') }}">Usuaris</a></h2>
+            </button>
+        </div>
+    </div>
 @endsection
--- resources/views/backend/usuaris/index.blade.php
+++ resources/views/backend/usuaris/index.blade.php
@@ -5,44 +5,53 @@
     <i class="fa fa-angle-right"></i>
     <a href="{{ action('backend\Usuaris@index') }}">Usuari</a>
 @endsection
+
+@section('css')
+    <link href="{{ URL::asset('/css/backend/add.css') }}" rel="stylesheet">
+@endsection
 @section('content')
+
 
     <div class="container">
         {!!Form::model($usuari, ['action' => ['backend\Usuaris@update', $usuari->id], 'method' => 'post', 'class' => 'form-horizontal'])!!}
-            <fieldset>
-                <!-- Text input-->
-                <div class="form-group">
-                    <label class="col-md-4 control-label" for="user">Nom de l'usuari</label>
-                    <div class="col-md-4">
-                        <input id="user" name="user" type="text" placeholder="@if($usuari){{ $usuari->name }} @endif" class="form-control input-md" required>
+
+        <div class="paper">
+            <div class="paperfull">
+                <fieldset>
+                    <div class="form-group">
+                        <label class="col-md-4 control-label" for="user">Nom de l'usuari</label>
+                        <div class="col-md-4">
+                            <input id="user" name="user" type="text" placeholder="@if($usuari){{ $usuari->name }} @endif" class="form-control input-md" required>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-md-4 control-label" for="email">E-mail</label>
-                    <div class="col-md-4">
-                        <input id="email" name="email" type="text" placeholder="@if($usuari){{ $usuari->email }} @endif" class="form-control input-md" required>
+                    <div class="form-group">
+                        <label class="col-md-4 control-label" for="email">E-mail</label>
+                        <div class="col-md-4">
+                            <input id="email" name="email" type="text" placeholder="@if($usuari){{ $usuari->email }} @endif" class="form-control input-md" required>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-md-4 control-label" for="password">Password</label>
-                    <div class="col-md-4">
-                        <input id="password" name="password" type="password" placeholder="Escriu un nou password" class="form-control input-md" required>
+                    <div class="form-group">
+                        <label class="col-md-4 control-label" for="password">Password</label>
+                        <div class="col-md-4">
+                            <input id="password" name="password" type="password" placeholder="Escriu un nou password" class="form-control input-md" required>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-md-4 control-label" for="remember">Indici de contrasenya</label>
-                    <div class="col-md-4">
-                        <input id="remember" name="remember" type="text" placeholder="Escriu un indici de contraseña" class="form-control input-md" required>
+                    <div class="form-group">
+                        <label class="col-md-4 control-label" for="remember">Indici de contrasenya</label>
+                        <div class="col-md-4">
+                            <input id="remember" name="remember" type="text" placeholder="Escriu un indici de contraseña" class="form-control input-md" required>
+                        </div>
                     </div>
-                </div>
-                <!-- Button -->
-                <div class="form-group">
-                    <label class="col-md-4 control-label" for="singlebutton"></label>
-                    <div class="col-md-4">
-                        <button id="singlebutton" name="singlebutton" class="btn btn-primary">Actualizar</button>
+                    <!-- Button -->
+                    <div class="form-group">
+                        <label class="col-md-4 control-label" for="singlebutton"></label>
+                        <div class="col-md-4">
+                            <button id="singlebutton" name="singlebutton" class="btn btn-primary">Actualizar</button>
+                        </div>
                     </div>
-                </div>
-            </fieldset>
+                </fieldset>
+            </div>
+        </div>
         {!!Form::close()!!}
 
     </div>
